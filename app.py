8bab3a232a638211d4d8f728c1af4a7db36ba079
Emily Atkins <emilyalice@Emilys-MacBook-Pro-5.local>
2017-07-17 19:59:20 +0100

    move files and add app

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask ("MyApp")
+
+@app.route("/")
+def hello():
+    return render_template("Googal.html")
+
+@app.route("/end")
+def hello():
+    return render_template("form.html")
+
+@app.route("/signup", methods=["POST"])
+def sign_up():
+    form_data = request.form
+    print form_data["email"]
+    return "ALL OK"
+
+app.run(debug=True)
